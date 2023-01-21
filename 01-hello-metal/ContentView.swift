d3b0d23e1a08789bddb02b8cc51e21062bb107f6
changhyun.jeon <changhyun.jeon@linecorp.com>
2023-01-22 00:09:13 +0900

    create hello metal project for tutorial

--- 01-hello-metal/ContentView.swift
+++ 01-hello-metal/ContentView.swift
@@ -10,10 +10,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+          MetalView()
+            .border(Color.black, width: 2)
+          Text("Hello, Metal!")
         }
         .padding()
     }
--- 01-hello-metal/_1_hello_metalApp.swift
+++ 01-hello-metal/_1_hello_metalApp.swift
@@ -1,6 +1,6 @@
 //
-//  _1_hello_worldApp.swift
-//  01-hello-world
+//  _1_hello_metalApp.swift
+//  01-hello-metal
 //
 //  Created by user on 2023/01/21.
 //
@@ -8,7 +8,7 @@
 import SwiftUI
 
 @main
-struct _1_hello_worldApp: App {
+struct _1_hello_metalApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
