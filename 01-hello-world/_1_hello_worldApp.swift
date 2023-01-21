f06dd7602f7e96497d51cdca9bfab7cd93685848
changhyun.jeon <changhyun.jeon@linecorp.com>
2023-01-21 23:53:29 +0900

    create proejct for hello world

---
+++ 01-hello-world/_1_hello_worldApp.swift
@@ -0,0 +1,17 @@
+//
+//  _1_hello_worldApp.swift
+//  01-hello-world
+//
+//  Created by user on 2023/01/21.
+//
+
+import SwiftUI
+
+@main
+struct _1_hello_worldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
