7c49e12775fa8861c8af9c30e8e29729960f1a1e
gandis0713 <gandis0713@gmail.com>
2023-02-01 21:53:25 +0900

    chapter: create 10 light fundamentals

---
+++ c10-light-fundamentals/C10LightFundamentals.swift
@@ -0,0 +1,17 @@
+//
+//  c10_light_fundamentalsApp.swift
+//  c10-light-fundamentals
+//
+//  Created by user on 2023/02/01.
+//
+
+import SwiftUI
+
+@main
+struct C10LightFundamentals: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
