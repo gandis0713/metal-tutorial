d92b8a34c79ed30c7650d0064cbb49cdf54e824f
gandis0713 <gandis0713@gmail.com>
2023-02-02 09:12:31 +0900

    refac: move static variable for input into InputContoller

--- c10-light-fundamentals/Game/Camera.swift
+++ c10-light-fundamentals/Game/Camera.swift
@@ -31,6 +31,7 @@
 /// THE SOFTWARE.
 
 import CoreGraphics
+import OSLog
 
 protocol Camera: Transformable {
     var projectionMatrix: float4x4 { get }
@@ -109,10 +110,13 @@ struct ArcballCamera: Camera {
     mutating func update(deltaTime: Float) {
         let input = InputController.shared
         let scrollSensitivity = Settings.mouseScrollSensitivity
+        os_log(.error, log: OSLog.error, "scroll: %f", scrollSensitivity)
         distance -= (input.mouseScroll.x + input.mouseScroll.y)
             * scrollSensitivity
+        os_log(.error, log: OSLog.error, "distance: %f", distance)
         distance = min(maxDistance, distance)
         distance = max(minDistance, distance)
+        os_log(.error, log: OSLog.error, "distance2: %f", distance)
         input.mouseScroll = .zero
         if input.leftMouseDown {
             let sensitivity = Settings.mousePanSensitivity
